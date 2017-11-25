cdb477d2f39f292bd3899d69cb12643ac58e24e2
rosem <michael_rose@gmx.de>
2017-11-25 18:03:35 +0100

    add support for chome building (different manifest + polyfill)

--- build/manifest.js
+++ build/manifest.js
@@ -2,11 +2,11 @@
 
 const fs = require('fs');
 
-if (process.argv.length < 3) {
-    throw new Error('missing platform')
+const platform = process.argv.length >= 3 ? process.argv[2] : process.env.CURLIFY_PLATFORM;
+if (!platform) {
+    throw new Error('missing platform');
 }
 
-const platform = process.argv[2];
 const platformJson = `manifest.${platform}.json`;
 
 if (!fs.existsSync('manifest.base.json')) {
@@ -16,3 +16,9 @@ if (!fs.existsSync('manifest.base.json')) {
 if (!fs.existsSync(platformJson)) {
     throw new Error(`missing ${platformJson}`);
 }
+
+const baseData = JSON.parse(fs.readFileSync('manifest.base.json', { encoding: 'UTF-8' }));
+const platformData = JSON.parse(fs.readFileSync(platformJson, { encoding: 'UTF-8' }));
+
+const resultData = Object.assign({}, baseData, platformData);
+fs.writeFileSync('manifest.json', JSON.stringify(resultData), { encoding: 'UTF-8' });
